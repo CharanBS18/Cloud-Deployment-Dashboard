aadfd1a29009f78428929ac44b86a8eb39ae5fc9
CharanBS18 <charan201204@gmail.com>
2026-05-28 14:34:55 +0530

    chore: configure GitHub Pages deployment

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -4,4 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+
+  // REQUIRED for GitHub Pages deployment
+  base: '/Cloud-Deployment-Dashboard/',
 })
